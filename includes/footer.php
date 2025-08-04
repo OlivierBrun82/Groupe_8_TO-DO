6c69175a52e8b370a0eacfe03e6fc40b93df0332
edouard-todorov-formation <edouard.todorov@laplateforme.io>
2025-08-04 02:57:53 +0200

    v1.0

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <title>footer</title>
-
-
-
-
-<footer>
-    <span class="title">@ma to-do list super utile</span>
-</footer>
+    <footer>
+        <p class="title">@ma to-do list super utile</p>
+    </footer>
+</body>
+</html>
--- includes/main.php
+++ includes/main.php
@@ -1,30 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <title>main</title>
-</head>
-
-<main>
-    <form action="" method="post">
-        <input type="text" name="task" placeholder="Ajouter une nouvelle tâche ici"  required>
-        <select name="category" required>
-            <option value="">Choisiser une catégorie</option>
-            <?php
-                foreach ($categories as $category) {
-                    echo '<option value="' . htmlspecialchars($category) . '">' . '</option>';
-                }
-            ?>
-        </select>
-    </form>
-    <h2 class="title">MES TO-DO LIST</h2>
+<!---------------------------------------------------------------------------------->
+<!-------------------------- FORMULAIRE -------------------------------------------->
+<!---------------------------------------------------------------------------------->
+    <main>
+        <form class="formContainer" action="" method="POST">
+            <div>
+                <input class="inputArea" type="text" name="task" placeholder="Ajouter une nouvelle tâche ici"  required> <!-- input pour entrer une tache -->
+                <select class="selectMenu" name="category" required>           <!-- menu déroulant pour selectionner un tableau de tache -->
+                    <option value="course">Course</option>
+                    <option value="travail">Travail</option>
+                    <option value="perso">Perso</option>
+                </select>
+            </div>
+            <button class="submitBtn" type="submit">Ajouter</button>
+        </form>
+<!---------------------------------------------------------------------------------->
 <!-------------------------- MES TABLEAUX TO DO LIST ------------------------------->
-    <div>
-        <h3 class="title">Liste de course</h3>
-        <div>
-
+<!---------------------------------------------------------------------------------->
+        <!-- <h2 class="title">MES TO-DO LIST</h2> -->
+<!-------------------------- LES TABLEAUX ------------------------------------------>
+        <div class="todosContainer">
+            <?php foreach ($_SESSION["todos"] as $category => $tasks) { ?> <!-- Boucle sur chaque catégorie en incluant les taches qu'on leur a assigné -->
+                <section class="todoContainer">
+                    <h3 class="title todoTitle"><?= $category ?></h3>
+                    <?php if (empty($tasks)) { ?>
+                        <p class="empty">Aucune tâche pour cette catégorie.</p>
+                    <?php } else { ?>
+                        <ul>
+                            <?php foreach ($tasks as $index => $task) { ?>
+                                <li>
+                                    <input type="checkbox">
+<!-- ---------------------------------------------------------------------------------------------------------------------- -->
+<!----------------------------- a ajouter si sauvegarde des taches coché ----------------------------------------------------->
+                                <!-- <input -->
+                                    <!-- type="checkbox" -->
+                                    <!-- name="done[<//?= htmlspecialchars($category) ?>][]" -->
+                                    <!-- value="<//?= $index ?>" -->
+                                    <!-- <//?= $task['done'] ? 'checked' : '' ?> -->
+                                >
+                                    <a class="delete-btn" href="?delete=<?= $index ?>&category=<?= $category ?>">🗑</a>
+                                    <span><?= htmlspecialchars($task) ?></span>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                    <?php } ?>
+                </section>
+            <?php } ?>
         </div>
-    </div>
-</main>
+    </main>
+</body>
+</html>
